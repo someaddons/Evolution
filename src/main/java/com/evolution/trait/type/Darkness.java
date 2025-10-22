bf13f3ce62923b78669e075e6d8f1ca99680f3be
someaddons <someaddons@github.com>
2025-10-22 17:59:22 +0200

    1.6: Allow darkness trait to appear outside of mutation on hostile mobs

--- src/main/java/com/evolution/trait/type/Darkness.java
+++ src/main/java/com/evolution/trait/type/Darkness.java
@@ -7,7 +7,12 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.level.Level;
 
 /**
@@ -21,7 +26,7 @@ public class Darkness implements ITraitType
     /**
      * Appareance chance
      */
-    private int chance = 0;
+    private int chance = 2;
 
     public Darkness()
     {
@@ -49,7 +54,18 @@ public class Darkness implements ITraitType
     @Override
     public boolean isCompatibleEntityType(final EntityType type, final Level level)
     {
-        return true;
+        if (type.is(compatible))
+        {
+            return true;
+        }
+
+        final Entity entity = type.create(level);
+        if (entity instanceof Enemy)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
